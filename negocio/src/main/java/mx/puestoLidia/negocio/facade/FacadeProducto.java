10236b1440e6b7756a727f00f117b1aee5630b8d
Mariel <mariel.torres@uabc.edu.mx>
2026-04-20 00:18:41 -0700

    capa negocio terminada

--- negocio/src/main/java/mx/puestoLidia/negocio/facade/FacadeProducto.java
+++ negocio/src/main/java/mx/puestoLidia/negocio/facade/FacadeProducto.java
@@ -1,4 +1,22 @@
 package mx.puestoLidia.negocio.facade;
 
+import mx.puestoLidia.entity.Producto;
+import mx.puestoLidia.negocio.delegate.DelegateProducto;
+
 public class FacadeProducto {
+    private final DelegateProducto delegateProducto;
+
+    public FacadeProducto( ) {
+        this.delegateProducto = new DelegateProducto();
+    }
+
+    // alta de un producto
+    public void guardarProducto(Producto producto){
+        delegateProducto.guardarProducto(producto);
+    }
+
+    // obtener un producto por su id
+    public Producto buscarProductoPorID(String idBuscar){
+        return delegateProducto.buscarProductoPorID(idBuscar);
+    }
 }
