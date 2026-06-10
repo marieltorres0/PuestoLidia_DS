9714494d3bb1c46af640cf69e3a86c6e28bbd467
Mariel Torres Camacho <mariel.torres@uabc.edu.mx>
2026-06-10 15:01:26 -0700

    actualizacion

--- vista/src/main/java/ui/ProductoBeanUI.java
+++ vista/src/main/java/ui/ProductoBeanUI.java
@@ -262,14 +262,6 @@ public class ProductoBeanUI implements Serializable {
                     new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Ingresa una cantidad mayor a 0."));
             return;
         }
-    // =========================================================
-    // MÉTODO PARA ALIMENTAR LA TABLA INVISIBLE DEL PDF
-    // =========================================================
-    public List<Producto> getListaProductosCriticos() {
-        return productoHelper.obtenerReporteStockCritico();
-    }
-
-
         try {
             // Hacer la suma matemática
             int nuevaCantidad = productoSeleccionado.getCantidad() + cantidadEntrada;
@@ -292,6 +284,13 @@ public class ProductoBeanUI implements Serializable {
             e.printStackTrace();
         }
     }
+    // =========================================================
+    // MÉTODO PARA ALIMENTAR LA TABLA INVISIBLE DEL PDF
+    // =========================================================
+    public List<Producto> getListaProductosCriticos() {
+        return productoHelper.obtenerReporteStockCritico();
+    }
+
 
     // ================= GETTERS Y SETTERS =================
 
