7b4f19b986094836eed344a9feae1db2438e41ce
Eduardo Avitia <chido.eac@gmail.com>
2026-06-16 01:30:23 -0700

    Esta son las capas de negocio

--- negocio/src/main/java/mx/puestoLidia/negocio/integration/ServiceFacadeLocator.java
+++ negocio/src/main/java/mx/puestoLidia/negocio/integration/ServiceFacadeLocator.java
@@ -1,33 +1,45 @@
 package mx.puestoLidia.negocio.integration;
 
-import mx.puestoLidia.negocio.facade.FacadeItemVenta;
 import mx.puestoLidia.negocio.facade.FacadeProducto;
 import mx.puestoLidia.negocio.facade.FacadeVenta;
+import mx.puestoLidia.negocio.facade.FacadeItemVenta;
+import mx.puestoLidia.negocio.facade.FacadeCliente;
 
+/**
+ * ServiceFacadeLocator - Patrón Singleton - Acceso único a todas las Facades
+ */
 public class ServiceFacadeLocator {
 
     private static FacadeProducto facadeProducto;
     private static FacadeVenta facadeVenta;
     private static FacadeItemVenta facadeItemVenta;
+    private static FacadeCliente facadeCliente;
 
-    public static FacadeProducto getInstanceFacadeProducto(){
-        if (facadeProducto == null){
+    public static FacadeProducto getInstanceFacadeProducto() {
+        if (facadeProducto == null) {
             facadeProducto = new FacadeProducto();
         }
         return facadeProducto;
     }
 
-    public static FacadeVenta getInstanceFacadeVenta(){
-        if (facadeVenta == null){
+    public static FacadeVenta getInstanceFacadeVenta() {
+        if (facadeVenta == null) {
             facadeVenta = new FacadeVenta();
         }
         return facadeVenta;
     }
-    public static FacadeItemVenta getInstanceFacadeItemVenta(){
-        if (facadeItemVenta == null){
+
+    public static FacadeItemVenta getInstanceFacadeItemVenta() {
+        if (facadeItemVenta == null) {
             facadeItemVenta = new FacadeItemVenta();
         }
         return facadeItemVenta;
     }
 
+    public static FacadeCliente getInstanceFacadeCliente() {
+        if (facadeCliente == null) {
+            facadeCliente = new FacadeCliente();
+        }
+        return facadeCliente;
+    }
 }
